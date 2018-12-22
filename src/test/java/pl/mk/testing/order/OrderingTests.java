0c74898479aa8c6eeaab054067c2de794c298a82
Maciek Kurek <kurekmaciek@gmail.com>
2018-12-22 18:13:51 +0100

    OrderingTests

---
+++ src/test/java/pl/mk/testing/order/OrderingTests.java
@@ -0,0 +1,34 @@
+package pl.mk.testing.order;
+
+import org.junit.AfterClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrderingTests {
+
+    private static StringBuilder output = new StringBuilder("");
+
+    @Test
+    public void secondTest() {
+        output.append("b");
+    }
+
+    @Test
+    public void thirdTest() {
+        output.append("c");
+    }
+
+    @Test
+    public void firstTest() {
+        output.append("a");
+    }
+
+    @AfterClass
+    public static void assertOutput() {
+        assertEquals(output.toString(), "cab");
+    }
+}
